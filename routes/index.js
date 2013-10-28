1e067ba0e1ed2eec81a89004cd00f54662e78b26
Stephane Becker <sbecker@methodinthemadness.eu>
2013-10-28 22:55:01 +0100

    - remove the debug log - generalise the hack

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@
 exports.index = function(req, res){
 
     // this is ugly, but firefox made me do it.
-    console.log( req.headers['user-agent'] );
     if( req.headers['user-agent'].indexOf("Firefox") !== -1 ) {
         res.render('index', { chartWidth: '1170px' } );
     } else {
